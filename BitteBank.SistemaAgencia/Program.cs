bee9c60a6fda80e98d0872a99ca1cf9568271840
André Miranda <andrelmmiranda@gmail.com>
2022-06-04 13:48:25 -0300

    modificando estrutura do projeto

---
+++ BitteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BitteBank.Modelos;
+
+namespace BitteBank.SistemaAgencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente cc = new ContaCorrente(new Cliente("André", "1"), 1, 1);
+
+            Console.WriteLine(cc.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
