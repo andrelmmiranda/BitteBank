1e2c92a035b60fb2f703bf493dc7e298460d758c
André Miranda <andrelmmiranda@gmail.com>
2022-06-05 22:48:03 -0300

    adicionando AutenticacaoHelper

---
+++ BitteBank.Modelos/AutenticacaoHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitteBank.Modelos
+{
+    internal class AutenticacaoHelper
+    {
+        public bool CompararSenha(string senha, string senhaTentativa)
+        {
+            return senha == senhaTentativa;
+        }
+    }
+}
